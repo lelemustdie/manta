42aca66cd3c94fc3e55c5d132dae6e452cd52cbb
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-05-11 00:43:28 -0300

    fix navbar on safari

--- components/template/navbar/NavBar.tsx
+++ components/template/navbar/NavBar.tsx
@@ -1,9 +1,8 @@
 "use client"
 import Hamburguer from "@/assets/icons/hamburguer"
-import Logo from "@/assets/icons/logo"
 import Link from "next/link"
 import { AnimatePresence, AnimationProps, motion } from "framer-motion"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const NAVBAR_ITEMS = [
     { title: "SL3", href: "/sl3" },
@@ -31,17 +30,47 @@ const animations: AnimationProps = {
 
 export default function NavBar({ withTransition = false }) {
     const [isOpen, setIsOpen] = useState(false)
+    useEffect(() => {
+        const isSafari = /^((?!chrome|android).)*safari/i.test(
+            navigator.userAgent,
+        )
+        if (isSafari && withTransition) {
+            const handleScroll = () => {
+                const navbar = document.querySelector("nav")
+                const navbarHeightClass = "!h-[100px]"
+                const navbarColorClass = "bg-manta-ms-black"
+                const shouldBeToggled = window.scrollY > window.innerHeight / 2
+
+                // Agregar o quitar las clases según corresponda
+                if (shouldBeToggled) {
+                    navbar?.classList.add(navbarHeightClass, navbarColorClass)
+                    navbar?.classList.remove("bg-transparent")
+                } else {
+                    navbar?.classList.remove(
+                        navbarHeightClass,
+                        navbarColorClass,
+                    )
+                }
+            }
+
+            window.addEventListener("scroll", handleScroll)
+
+            return () => window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
 
     return (
         <nav
-            className={`fixed left-0 top-0 z-10 flex w-full flex-row items-center justify-between border-b-2 border-manta-red px-[15%] font-semibold  ${
+            className={`fixed left-0 top-0 z-10 flex w-full flex-row items-center justify-between border-b-2 border-manta-red px-[15%] font-semibold duration-500  ${
                 withTransition
                     ? "navbar bg-transparent "
                     : "h-[100px] bg-manta-ms-black"
             }`}
         >
             <Link href="/">
-                <h1 className=" bg-gradient-to-r from-manta-red via-red-900 to-manta-red inline-block text-transparent bg-clip-text font-medium">AQUABIKE</h1>
+                <h1 className=" inline-block bg-gradient-to-r from-manta-red via-red-900 to-manta-red bg-clip-text font-medium text-transparent">
+                    AQUABIKE
+                </h1>
             </Link>
             <div className="flex flex-row gap-10 text-lg text-white  *:duration-300">
                 {NAVBAR_ITEMS.map(({ title, href }, index) => {
