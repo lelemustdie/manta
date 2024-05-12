7b178c588214e19b09575433dbc7440539a57431
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-05-12 03:38:34 -0300

    masonry

---
+++ app/(normal)/galeria/page.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image"
+import "./styles.css"
+
+const images = [
+    {
+        src: "/gallery/1.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/2.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/3.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/4.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/5.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/6.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/7.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/Father and Son riding the SL3.webp",
+        alt: "Father and son riding the SL3",
+    },
+    {
+        src: "/gallery/9.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/10.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/11.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/14.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/13.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/15.webp",
+        alt: "sl3",
+    },
+    {
+        src: "/gallery/16.webp",
+        alt: "sl3",
+    },
+]
+
+export default function Gallery() {
+    return (
+        <section className="gallery">
+            {images.map(({ src, alt }, index) => {
+                return (
+                    <div
+                        className="image-container"
+                        key={`gallery-image-${index}`}
+                    >
+                        <Image
+                            src={src}
+                            alt={alt}
+                            layout="fill"
+                            style={{ objectFit: "cover" }}
+                        />
+                    </div>
+                )
+            })}
+        </section>
+    )
+}
